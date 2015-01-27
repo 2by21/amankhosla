50049525689ab42dddf023995c985a4ae7642246
sunwooz <yangsunwoo@gmail.com>
2015-01-27 17:27:01 +0530

    Add ability to delete/update/edit videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,29 @@
 class VideosController < ApplicationController
+  def new
+    @video = Video.new
+  end
 
   def create
     @video = Video.new(video_params)
     if @video.save
+      redirect_to admin_path
+    end
+  end
+
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    if @video.update(video_params)
+      redirect_to admin_path
+    end
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    if @video.destroy
       redirect_to admin_path
     end
   end
