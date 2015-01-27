aaa1c9e46de5f84302eab57bdbfdac1543785b98
sunwooz <yangsunwoo@gmail.com>
2015-01-27 17:21:05 +0530

    Ability to edit/destroy/update posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,23 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to admin_path
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_path
+  end
+
   private
 
     def post_params
