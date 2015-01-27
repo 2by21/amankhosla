9d10596311409206c5671d2d6e961fd6dc89a731
sunwooz <yangsunwoo@gmail.com>
2015-01-27 17:34:50 +0530

    Delete unnecssary variables

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,9 +2,7 @@ class AdminsController < ApplicationController
 
   def index
     @posts = Post.all
-    @video = Video.new
     @videos = Video.all
     @shows = Show.all
-    @show = Show.new
   end
 end
