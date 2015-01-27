1fd2f328d1a5873d87a11b1f2106c22b63d67156
sunwooz <yangsunwoo@gmail.com>
2015-01-27 17:31:52 +0530

    Add ability to edit/update/destroy shows

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,9 +3,27 @@ class ShowsController < ApplicationController
     @show = Show.new
   end
 
+  def edit
+    @show = Show.find(params[:id])
+  end
+
+  def update
+    @show = Show.find(params[:id])
+    if @show.update(show_params)
+      redirect_to admin_path
+    end
+  end
+
   def create
     @show = Show.new(show_params)
     if @show.save
+      redirect_to admin_path
+    end
+  end
+
+  def destroy
+    @show = Show.find(params[:id])
+    if @show.destroy
       redirect_to admin_path
     end
   end
